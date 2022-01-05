70c8685cecf713c8e9c3f8d3e14e62fc829b696f
Getvion <gog0125@mail.ru>
2022-01-05 17:44:32 +0300

    create addList component

--- src/components/AddList/AddList.jsx
+++ src/components/AddList/AddList.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import s from './AddList.module.scss';
 
 function AddList(props) {
+  const [active, setActive] = useState(false);
+
   return (
-    <div className={s.addList}>
-      <button> Add new List</button>
-    </div>
+    <>
+      {active ? (
+        <div className={s.addlist}>
+          <input className={s.addlist__input} type='text' placeholder='Type your list name' />
+          <button className={s.addlist__button}>Add List</button>
+        </div>
+      ) : (
+        <div className={s.addlist} onClick={() => setActive(!active)}>
+          <img
+            className={s.addlist__img}
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Feather-core-plus-circle.svg/1200px-Feather-core-plus-circle.svg.png'
+            alt='plus'
+          />
+          <p className={s.addlist__text}>Add new List</p>
+        </div>
+      )}
+    </>
   );
 }
 
